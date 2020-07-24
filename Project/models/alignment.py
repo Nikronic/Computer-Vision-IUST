bd88ea37d8787fcf435246ebd59d0004da2edd33
M. Doosti Lakhani <nikan.doosti@outlook.com>
2020-07-24 15:22:28 +0430

    finish alignment using cv2

--- Project/models/alignment.py
+++ Project/models/alignment.py
@@ -6,18 +6,22 @@ import matplotlib.pyplot as plt
 from copy import deepcopy
 
 # %% read edited image (manually)
+from docutils.nodes import footnote_reference
+
 background = cv2.imread('data/images/room.jpg', 0)
-# top-left, bottom-left, bottom-right, top-right
-background_points = np.array([[300, 538],
-                              [385, 538],
+# bottom-left, top-left, bottom-right, top-right
+background_points = np.array([[385, 538],
+                              [300, 538],
                               [388, 689],
                               [294, 689]], dtype=np.float32)
+background_points = np.roll(background_points, 0, axis=1)
 
-foreground = cv2.imread('data/images/building1.jpg', 0)
-foreground_points = np.array([[0, 0],
-                              [foreground.shape[0], 0],
+foreground = cv2.imread('data/images/building2.jpg', 0)
+foreground_points = np.array([[foreground.shape[0], 0],
+                              [0, 0],
                               [foreground.shape[0], foreground.shape[1]],
                               [0, foreground.shape[1]]], dtype=np.float32)
+foreground_points = np.roll(foreground_points, 1, axis=1)
 
 h, mask = cv2.findHomography(foreground_points, background_points, cv2.RANSAC)
 im1reg = cv2.warpPerspective(foreground, h, background.shape)
